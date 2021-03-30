026f73cb4d5ccfdec0f5906b6abf7ee78f48187e
vulnerable-coder <malays.ece17@itbhu.ac.in>
2021-03-30 15:25:53 +0530

    Update String_Calculator.java
    Step 2&3:handle unknown amount of input and take \n as a delimiter as well with comma

--- String_Calculator.java
+++ String_Calculator.java
@@ -3,24 +3,21 @@ class String_Calculator
 {
     public int Add(String n)
     {int k=0;
-        if(n.length()!=0)
-       {
-           if(n.indexOf(',')==-1)
-           k=Integer.parseInt(n);
-           else
-           {
-               int a=Integer.parseInt(n.substring(0,n.indexOf(',')));
-               int b=Integer.parseInt(n.substring(n.indexOf(',')+1,n.length()));
-               k=a+b;
-           }
-       }
+       String[] na=n.split("[, ]");
+       for(String a: na)
+       k+=Integer.parseInt(a);
         return k;
 
     }
     public static void main(String ar[])
     {
         Scanner in=new Scanner(System.in);
-        String num=in.nextLine();
+        String num="";
+        String k=" ";
+        
+        while(k.equals("")==false)
+       {k=in.nextLine();
+    num+=k+" ";}
         String_Calculator ob=new String_Calculator();
         System.out.println(ob.Add(num));
         in.close();
