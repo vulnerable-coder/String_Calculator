d7e68707bee554dec5109f270533f7d735ffb43b
vulnerable-coder <malays.ece17@itbhu.ac.in>
2021-03-30 18:09:03 +0530

    Update String_Calculator.java
    Step 4 : delimiter of choice can be placed in the start of the String in case no delimiter placed error throws

--- String_Calculator.java
+++ String_Calculator.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class String_Calculator
 {
-    public int Add(String n)
+    public int Add(String n,String x)
     {int k=0;
-       String[] na=n.split("[, ]");
+       String[] na=n.split(x);
        for(String a: na)
        k+=Integer.parseInt(a);
         return k;
@@ -13,13 +13,20 @@ class String_Calculator
     {
         Scanner in=new Scanner(System.in);
         String num="";
-        String k=" ";
-        
-        while(k.equals("")==false)
+       // String k=" ";
+        String x="";
+        num=in.nextLine();
+      //  if(num.length()==0)
+       // k="";
+         if(num.substring(0,2).equals("//"))
+      {  x=""+num.charAt(2);
+    num="";} 
+    num=in.nextLine();
+       /* while(k.equals("")==false)
        {k=in.nextLine();
-    num+=k+" ";}
+    num+=k+x;}*/
         String_Calculator ob=new String_Calculator();
-        System.out.println(ob.Add(num));
+        System.out.println(ob.Add(num,x));
         in.close();
     }
 }
